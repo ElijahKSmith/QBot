1e47cb8cbe047122e2f2df7bba02a54ee87841be
ElijahKSmith <squidtheprinter@gmail.com>
2020-03-30 15:14:15 -0400

    Added a database creation file to recreate the db

---
+++ create_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('server.db')
+
+conn.execute('''CREATE TABLE verified(
+             discordId  TEXT PRIMARY KEY NOT NULL,
+             summoner   TEXT             NOT NULL
+             summonerId TEXT             NOT NULL,
+             accountId  TEXT             NOT NULL,
+             puuid      TEXT             NOT NULL
+             );''')
+
+conn.execute('''CREATE TABLE unverified(
+             discordId TEXT PRIMARY KEY NOT NULL,
+             summoner  TEXT             NOT NULL
+             );''')
+
+conn.close()
