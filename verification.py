d5d1d56cd7db99e48581b583da50c2f8388482e1
ElijahKSmith <squidtheprinter@gmail.com>
2020-04-02 16:54:19 -0400

    Added error checking for registration command

--- verification.py
+++ verification.py
@@ -7,9 +7,18 @@ class Verification(commands.Cog, name="Verification"):
 
     @commands.command()
     @commands.check(checks.is_unverified)
+    @commands.guild_only()
     async def register(self, ctx):
         member = ctx.author.id
         await ctx.send("Not yet implemented")
+        await ctx.send("Test")
+
+    @register.error
+    async def register_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            pass
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.send('You cannot register, you may be already registered or currently in the verification process.')
 
 def setup(bot):
     bot.add_cog(Verification(bot))
